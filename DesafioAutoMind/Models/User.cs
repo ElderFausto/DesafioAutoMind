eee5154cc630d584679c2f7f077fed47940737cc
unknown <elderfavsto@gmail.com>
2025-03-18 00:26:19 -0300

    feat: cria o usuario

---
+++ DesafioAutoMind/Models/User.cs
@@ -0,0 +1,22 @@
+namespace DesafioAutoMind.Models
+{
+  public class User
+  {
+    // cria as variaveis para receber os parametros de usuario
+    public string Name { get; }
+    public string Email { get; }
+    public int Age { get; }
+
+    public User(string name, string email, int age)
+    {
+      Name = name;
+      Email = email;
+      Age = Age;
+    }
+    // retorna os parametros passados pelo usuario
+    public override string ToString()
+    {
+      return $"Nome: {Name}, Email: {Email}, Idade: {Age}";
+    }
+  }
+}
